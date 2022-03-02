a86b8c3ea17a1402a0ec948d3268b5a5e61b1cbf
Awaab Elamin <AwaabElamin@gmail.com>
2022-03-02 16:10:19 -0600

    add testing methode in main class

--- src/main/java/com/revature/foodMartApi/FoodMartApiApplication.java
+++ src/main/java/com/revature/foodMartApi/FoodMartApiApplication.java
@@ -2,10 +2,14 @@ package com.revature.foodMartApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FoodMartApiApplication {
 
+	@GetMapping("/test")
 	public static void main(String[] args) {
 		SpringApplication.run(FoodMartApiApplication.class, args);
 	}
